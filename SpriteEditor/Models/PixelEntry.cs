306da341d7a24059c736346dcb0ac7e3f5ec5ae4
al_savant@hotmail.com <al_savant@hotmail.com>
2022-11-05 18:38:19 +0200

    Sprite editor.

---
+++ SpriteEditor/Models/PixelEntry.cs
@@ -0,0 +1,52 @@
+﻿using SpriteEditor.Models;
+
+namespace SpriteEditor.ViewModels
+{
+    public class PixelEntry : ViewModel
+    {
+        public static PixelEntry Default
+        {
+            get
+            {
+                var entry = new PixelEntry();
+                entry.Character = ' ';
+                entry.Color = ColorEntry.FromConsoleColor(System.ConsoleColor.Black);
+                return entry;
+            }
+        }
+
+        private char character;
+        public char Character
+        {
+            get
+            {
+                return character;
+            }
+            set
+            {
+                SetProperty(ref character, value, "Character");
+            }
+        }
+
+        private ColorEntry color;
+        public ColorEntry Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                SetProperty(ref color, value, "Color");
+            }
+        }
+
+        public PixelEntry Clone()
+        {
+            var clone = new PixelEntry();
+            clone.Character = Character;
+            clone.Color = Color;
+            return clone;
+        }
+    }
+}
